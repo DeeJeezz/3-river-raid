77965af40b45cd6c56023b7d6d07f24eecc40388
DeeJeezz <killmaster00431@gmail.com>
2026-06-07 14:54:49 +0500

    reworked boats and cannons visuals

--- scripts/boat.gd
+++ scripts/boat.gd
@@ -1,22 +1,35 @@
+@tool
+class_name Boat
 extends Area2D
 
 var _destroying: bool = false
 
+
+@export_enum("1", "2", "3") var boat_color: String = "1":
+	set(value):
+		boat_color = value
+		boat_sprite.animation = value
+		cannon_sprite.animation = value
+@export_range(-360, 360, 1) var cannon_rotation: float = 0:
+	set(value):
+		cannon_rotation = value
+		cannon_sprite.rotation_degrees = cannon_rotation
+
 @onready var explosion: Explosion = $Explosion
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var shadow_sprite: Sprite2D = $Shadow
+@onready var boat_sprite: AnimatedSprite2D = $Boat
+@onready var cannon_sprite: AnimatedSprite2D = $Boat/Cannon
 
 
 func _ready() -> void:
-	animated_sprite.play(Utils.get_random_animation_name(animated_sprite))
+	boat_color = Utils.get_random_animation_name(boat_sprite)
+	boat_sprite.play()
 
 
 func _destroy() -> void:
 	if _destroying:
 		return
-	animated_sprite.hide()
-	shadow_sprite.hide()
+	boat_sprite.hide()
 	_destroying = true
 	collision_shape.set_deferred(&"disabled", true)
 	explosion.play_random_explosion()
